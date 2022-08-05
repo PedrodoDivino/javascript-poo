208e4ad6f39db290550c85ce0af0d2c78838077e
PedrodoDivino <pedrohenriquebatera@hotmail.com>
2022-08-05 17:34:16 -0300

    [Pedro] começando o curso

---
+++ aulas Alura/index.js
@@ -0,0 +1,35 @@
+
+
+class Client {
+
+    name;
+    cpf;
+    agency;
+    balance;
+}
+
+client1 = new Client();
+
+const client2 = new Client();
+
+
+ client1.name = 'Ricardo';
+
+client1.Cpf = 1233454545;
+
+ client1.Agency =  1001;
+
+ client1.Balance =  0;
+
+const client2name = 'Alice';
+
+const cliient2Cpf = 98764321;
+
+const client2Agency =  1001;
+
+const client2Balance =  0;
+
+
+console.log(client1);
+
+
